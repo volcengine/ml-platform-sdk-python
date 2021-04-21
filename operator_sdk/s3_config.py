dfe4b78987238d9fa2844648220d3754c808df06
Wei Wei <weiwei.ethan@bytedance.com>
2021-04-21 20:09:09 +0800

    fix code style

--- operator_sdk/s3_config.py
+++ operator_sdk/s3_config.py
@@ -1,4 +1,5 @@
 region_name = 'cn-qingdao'
 endpoint_url = 'http://tos-s3-cn-qingdao.volces.com'
 aws_access_key_id = 'AKLTZTA4OWEzMGRjYTdlNDIzNzlhMjQ1YjJiMTcxN2MxMDg'
+# pylint: disable=C0301
 aws_secret_access_key = 'WVdJMVpUUm1OR1ptTnpJM05EYzNPV0prWTJNellXTTFOR1F3WldObU1ETQ=='
--- operator_sdk/tos_client.py
+++ operator_sdk/tos_client.py
@@ -1,8 +1,8 @@
 import logging
 import boto3
-from s3_config import region_name, aws_access_key_id, aws_secret_access_key, endpoint_url
 from botocore.exceptions import ClientError
-from botocore.config import Config
+from s3_config import region_name, aws_access_key_id, \
+aws_secret_access_key, endpoint_url
 
 
 class TOSClient:
@@ -12,17 +12,18 @@ class TOSClient:
 
     def _init_boto3_client(self):
         # custom config
+        # pylint: disable=C0301
         # ref: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html
         client = boto3.client(
             's3',
             region_name=region_name,
             endpoint_url=endpoint_url,
             aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,)
+            aws_secret_access_key=aws_secret_access_key,
+        )
         return client
 
-
-    def create_bucket(bucket_name, region):
+    def create_bucket(self, bucket_name, region):
         """Create an S3 bucket in a specified region
 
         :param bucket_name: Bucket to create
@@ -32,10 +33,13 @@ class TOSClient:
 
         # Create bucket
         try:
-            s3_client = boto3.client('s3', region_name= "<tosRegionName>", 
-                                    endpoint_url="<tosDomainName>",
-                                    aws_access_key_id="<yourAccessKey>",
-                                    aws_secret_access_key="<yourSecretKey>", )
+            s3_client = boto3.client(
+                's3',
+                region_name="<tosRegionName>",
+                endpoint_url="<tosDomainName>",
+                aws_access_key_id="<yourAccessKey>",
+                aws_secret_access_key="<yourSecretKey>",
+            )
             location = {'LocationConstraint': region}
             s3_client.create_bucket(Bucket=bucket_name,
                                     CreateBucketConfiguration=location)
@@ -44,10 +48,10 @@ class TOSClient:
             return False
         return True
 
-    def delete_bucket(self, bucket_name):
+    def delete_bucket(self, bucket):
         """Delete a bucket, an error will be raised if it is not empty"""
         try:
-            response = s3.delete_bucket(Bucket="examplebucket")
+            response = self.s3_clientdelete_bucket(Bucket=bucket)
             return True
         except ClientError as e:
             logging.error(e)
@@ -57,7 +61,8 @@ class TOSClient:
         """List S3 buckets
 
         return a list of Bucket infos
-        Example: [{'Name': 'test', 'CreationDate': datetime.datetime(2021, 3, 19, 6, 37, 40, tzinfo=tzutc())}]
+        Example: [{'Name': 'test', 'CreationDate':
+        datetime.datetime(2021, 3, 19, 6, 37, 40, tzinfo=tzutc())}]
         """
         return self.s3_client.list_buckets()['Buckets']
 
@@ -75,6 +80,3 @@ class TOSClient:
     def get_object(self, bucket, key):
         """Download single object"""
         return self.s3_client.get_object(Bucket=bucket, Key=key)['Body']
-
-
-
