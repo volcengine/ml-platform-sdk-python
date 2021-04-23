59840ebc8d307327efeeee4a6b17d080e1589d1b
Wei Wei <weiwei.ethan@bytedance.com>
2021-04-23 17:32:55 +0800

    change Slot object logic

--- operator_sdk/io.py
+++ operator_sdk/io.py
@@ -3,29 +3,32 @@ from operator_sdk import env
 
 class Slot:
 
-    def __init__(self):
+    def __init__(self, slot_index: int, io_type: str):
         self._config = env.NodeConfig()
 
-    def get_output_path(self, slot_index: int) -> str:
-        """Convert output slot_id to local storage path"""
-        if slot_index < 1 or slot_index > len(self._config.get_output_slots()):
-            raise ValueError('wrong slot_index value,\
-                 should be in the range [1, num_of_slots]')
-        slot_property = None
-        for s in self._config.get_output_slots():
-            if s['index'] == slot_index:
-                slot_property = s
-                break
-        return slot_property['path']
+        if io_type not in ('input', 'output'):
+            raise ValueError('io_type can only be "input" or "output"')
 
-    def get_input_path(self, slot_index: int) -> str:
-        """Convert input slot_id to local storage path"""
-        if slot_index < 1 or slot_index > len(self._config.get_input_slots()):
-            raise ValueError('wrong slot_index value,\
-                 should be in the range [1, num_of_slots]')
-        slot_property = None
-        for s in self._config.get_input_slots():
-            if s['index'] == slot_index:
-                slot_property = s
+        slots = None
+        if io_type == 'input' and 0 < slot_index <= len(
+                self._config.get_input_slots()):
+            slots = self._config.get_input_slots()
+        elif io_type == 'output' and 0 < slot_index <= len(
+                self._config.get_output_slots()):
+            slots = self._config.get_output_slots()
+        else:
+            raise ValueError('invalid slot_index value,\
+                              should be in the range [1, num_of_slots]')
+
+        self._index = slot_index
+        self._io_type = io_type
+        self._property = None
+
+        for s in slots:
+            if s['index'] == self._index:
+                self._property = s
                 break
-        return slot_property['path']
+
+    def get_path(self) -> str:
+        """return local storage path"""
+        return self._property['path']
