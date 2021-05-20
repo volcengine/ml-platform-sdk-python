ef1ef6942a495f4190fad2e1ea2dd46a9d8cf73e
Wei Wei <weiwei.ethan@bytedance.com>
2021-05-20 15:24:55 +0800

    change split dataset due to dataset

--- operator_sdk/dataset/split.py
+++ operator_sdk/dataset/split.py
@@ -7,26 +7,27 @@ import shutil
 import numpy as np
 
 
-def move_dataset(metadata, meta_type, input_dir, output_dir):
-    for data in metadata:
-        file_path = data['filePath']
-        file_dir, file_name = os.path.split(file_path)
-        target_dir = os.path.join(output_dir,
-                                  os.path.relpath(file_dir, start=input_dir))
+def move_dataset(metadata, input_dir, output_dir):
+    file_path = metadata['data']['filePath']
+    file_dir, file_name = os.path.split(file_path)
+    target_dir = os.path.join(output_dir,
+                              os.path.relpath(file_dir, start=input_dir))
 
-        # create output file directory
-        try:
-            os.makedirs(target_dir, exist_ok=True)
-        except OSError:
-            logging.warning('Cannot create directory: %s', target_dir)
+    # create output file directory
+    try:
+        os.makedirs(target_dir, exist_ok=True)
+    except OSError:
+        logging.warning('Cannot create directory: %s', target_dir)
 
-        target_file = os.path.join(target_dir, file_name)
-        shutil.copy(file_path, target_file)
-        data['filePath'] = target_file
+    target_file = os.path.join(target_dir, file_name)
+    shutil.copy(file_path, target_file)
+    metadata['data']['filePath'] = target_file
 
     # store metadata
-    with open(os.path.join(output_dir, 'metadata.json'), 'w+') as meta_file:
-        json.dump({meta_type: metadata}, meta_file)
+    with open(os.path.join(output_dir, 'local_metadata.manifest'),
+              'w+') as meta_file:
+        line = json.dumps(metadata)
+        meta_file.write(line)
 
 
 def split_dataset(input_dir,
@@ -34,23 +35,22 @@ def split_dataset(input_dir,
                   testing_dir,
                   ratio=0.8,
                   random_state=0):
-    with open(os.path.join(input_dir, 'metadata.json')) as f:
-        meta_json = json.load(f)
-        meta_type = None
-        if 'image_meta' in meta_json:
-            meta_type = 'image_meta'
-        elif 'video_meta' in meta_json:
-            meta_type = 'video_meta'
-        else:
-            meta_type = 'text_meta'
-        metadata = meta_json[meta_type]
+    line_count = 0
+    with open(os.path.join(input_dir, 'local_metadata.manifest')) as f:
+        for line in f:
+            line_count = line_count + 1
 
-        np.random.seed(random_state)
-        np.random.shuffle(metadata)
-
-        split_index = math.floor(len(metadata) * ratio)
-        training_metadata = metadata[:split_index]
-        testing_metadata = metadata[split_index:]
-
-    move_dataset(training_metadata, meta_type, input_dir, training_dir)
-    move_dataset(testing_metadata, meta_type, input_dir, testing_dir)
+    np.random.seed(random_state)
+    test_index_set = set(
+        np.random.choice(line_count,
+                         math.floor(line_count * (1 - ratio)),
+                         replace=False))
+    index = 0
+    with open(os.path.join(input_dir, 'local_metadata.manifest')) as f:
+        for line in f:
+            manifest_line = json.loads(line)
+            if index in test_index_set:
+                move_dataset(manifest_line, input_dir, testing_dir)
+            else:
+                move_dataset(manifest_line, input_dir, training_dir)
+            index = index + 1
