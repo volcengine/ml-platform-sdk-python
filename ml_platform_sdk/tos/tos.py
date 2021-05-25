2ced32d0b1d2fef0e3c3234e9155bf9324d64578
Wei Wei <weiwei.ethan@bytedance.com>
2021-05-25 15:25:46 +0800

    return datasets after split

--- ml_platform_sdk/tos/tos.py
+++ ml_platform_sdk/tos/tos.py
@@ -139,14 +139,20 @@ class TOSClient:
         if key is None:
             key = file_path
         # Set the desired multipart threshold value (20MB)
-        config = TransferConfig(multipart_threshold=part_size)
+        transfer_config = TransferConfig(multipart_threshold=part_size)
 
         # Perform the transfer
-        self.s3_client.upload_file(file_path, bucket, key, Config=config)
+        self.s3_client.upload_file(file_path,
+                                   bucket,
+                                   key,
+                                   Config=transfer_config)
 
     def download_file(self, file_path, bucket, key, max_concurrence=10):
         # To consume less downstream bandwidth, decrease the maximum concurrency
-        config = TransferConfig(max_concurrency=max_concurrence)
+        transfer_config = TransferConfig(max_concurrency=max_concurrence)
 
         # Download an S3 object
-        self.s3_client.download_file(bucket, key, file_path, Config=config)
+        self.s3_client.download_file(bucket,
+                                     key,
+                                     file_path,
+                                     Config=transfer_config)
