08b99ef89c7754400a6f24fbd5ab527d2ab2d3f6
Wei Wei <weiwei.ethan@bytedance.com>
2021-05-26 17:29:59 +0800

    add load data function for dataset class

--- demo/dataset_demo.py
+++ demo/dataset_demo.py
@@ -3,6 +3,9 @@ from ml_platform_sdk.dataset.dataset_service import DatasetService
 ak = 'AKLTOTk1NmEwOTYyZDQ2NGJmNTk5M2E1MWY4N2NmMzA4M2Q'
 sk = 'TnpjNFlUTmtZalZoTkRSaU5HRXdNV0l4TjJOaU9UWXlZekUxTnpBeE1tUQ=='
 region = 'cn-north-1'
+train_path = './train_dataset'
+test_path = './test_dataset'
+dataset_id = 'd-20210524180444-5fhsl'
 
 if __name__ == '__main__':
     client = DatasetService()
@@ -19,5 +22,7 @@ if __name__ == '__main__':
     # manifest = client.download_dataset('d-20210524144453-tr6mp', './')
 
     # Split Dataset
-    client.download_and_split_dataset('./train_dataset', './test_dataset',
-                                      'd-20210524144453-tr6mp')
+    client.download_and_split_dataset(train_path,
+                                      test_path,
+                                      dataset_id,
+                                      limit=100)
