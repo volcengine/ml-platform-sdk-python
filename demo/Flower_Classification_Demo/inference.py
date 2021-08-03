48ef59044fb22a0459de97ca63bf541e887f37a8
张杰坤 <zhangjiekun.zhangjk@bytedance.com>
2021-08-03 12:07:20 +0000

    feat(demo): swinTransformer

---
+++ demo/Flower_Classification_Demo/inference.py
@@ -0,0 +1,137 @@
+import json
+import requests
+import numpy as np
+from PIL import Image
+
+END_POINT = "inference-demo.byted.org/s-20210803143929-vfspk/rest"
+CLASSES = [
+    'pink primrose',
+    'hard-leaved pocket orchid',
+    'canterbury bells',
+    'sweet pea',
+    'wild geranium',
+    'tiger lily',
+    'moon orchid',
+    'bird of paradise',
+    'monkshood',
+    'globe thistle',  # 00 - 09
+    'snapdragon',
+    "colt's foot",
+    'king protea',
+    'spear thistle',
+    'yellow iris',
+    'globe-flower',
+    'purple coneflower',
+    'peruvian lily',
+    'balloon flower',
+    'giant white arum lily',  # 10 - 19
+    'fire lily',
+    'pincushion flower',
+    'fritillary',
+    'red ginger',
+    'grape hyacinth',
+    'corn poppy',
+    'prince of wales feathers',
+    'stemless gentian',
+    'artichoke',
+    'sweet william',  # 20 - 29
+    'carnation',
+    'garden phlox',
+    'love in the mist',
+    'cosmos',
+    'alpine sea holly',
+    'ruby-lipped cattleya',
+    'cape flower',
+    'great masterwort',
+    'siam tulip',
+    'lenten rose',  # 30 - 39
+    'barberton daisy',
+    'daffodil',
+    'sword lily',
+    'poinsettia',
+    'bolero deep blue',
+    'wallflower',
+    'marigold',
+    'buttercup',
+    'daisy',
+    'common dandelion',  # 40 - 49
+    'petunia',
+    'wild pansy',
+    'primula',
+    'sunflower',
+    'lilac hibiscus',
+    'bishop of llandaff',
+    'gaura',
+    'geranium',
+    'orange dahlia',
+    'pink-yellow dahlia',  # 50 - 59
+    'cautleya spicata',
+    'japanese anemone',
+    'black-eyed susan',
+    'silverbush',
+    'californian poppy',
+    'osteospermum',
+    'spring crocus',
+    'iris',
+    'windflower',
+    'tree poppy',  # 60 - 69
+    'gazania',
+    'azalea',
+    'water lily',
+    'rose',
+    'thorn apple',
+    'morning glory',
+    'passion flower',
+    'lotus',
+    'toad lily',
+    'anthurium',  # 70 - 79
+    'frangipani',
+    'clematis',
+    'hibiscus',
+    'columbine',
+    'desert-rose',
+    'tree mallow',
+    'magnolia',
+    'cyclamen ',
+    'watercress',
+    'canna lily',  # 80 - 89
+    'hippeastrum ',
+    'bee balm',
+    'pink quill',
+    'foxglove',
+    'bougainvillea',
+    'camellia',
+    'mallow',
+    'mexican petunia',
+    'bromelia',
+    'blanket flower',  # 90 - 99
+    'trumpet creeper',
+    'blackberry lily',
+    'common tulip',
+    'wild rose'
+]
+
+
+def request_demo():
+    try:
+        headers = {"content-type": "application/json"}
+        image = np.array(
+            Image.open(
+                '/root/code/Flower_Classification_Demo/test_sample/34.jpg'))
+        data = json.dumps({
+            "signature_name": "serving_default",
+            "inputs": [image.tolist()]
+        })
+        json_response = requests.post(
+            'http://{}/v1/models/default:predict'.format(END_POINT),
+            data=data,
+            headers=headers)
+        return json_response.json()
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    rv = request_demo()
+    preds = np.argmax(rv['outputs'], axis=-1)[0]
+    print(CLASSES[preds])
---
+++ demo/Flower_Classification_Demo/ml_platform_sdk/datasets/dataset.py
@@ -0,0 +1,128 @@
+import logging
+import os
+import shutil
+from typing import Optional, Tuple, List
+from urllib.parse import urlparse
+import json
+import requests
+
+from ml_platform_sdk import initializer
+from ml_platform_sdk.config import credential as auth_credential, constants
+from ml_platform_sdk.tos import tos
+from ml_platform_sdk.openapi import client
+
+
+def dataset_copy_file(metadata, source_dir, destination_dir):
+    file_path = metadata['Data']['FilePath']
+    file_dir, file_name = os.path.split(file_path)
+    target_dir = os.path.join(destination_dir,
+                              os.path.relpath(file_dir, start=source_dir))
+
+    # create output file directory
+    try:
+        os.makedirs(target_dir, exist_ok=True)
+    except OSError:
+        logging.warning('Cannot create directory: %s', target_dir)
+
+    target_file = os.path.join(target_dir, file_name)
+    shutil.copy(file_path, target_file)
+    metadata['Data']['FilePath'] = target_file
+
+
+class _Dataset:
+    """
+    datasets object
+    """
+
+    def __init__(self,
+                 dataset_id: Optional[str] = None,
+                 local_path: Optional[str] = None,
+                 tos_source: Optional[str] = None,
+                 credential: Optional[auth_credential.Credential] = None):
+        self.dataset_id = dataset_id
+        self.local_path = local_path
+        self.tabular_path = None
+        self.tos_source = tos_source
+        self.created = False
+        self.data_count = 0
+        self.detail = None
+        self.credential = credential or initializer.global_config.get_credential(
+        )
+        self.tos_client = tos.TOSClient(credential)
+        self.api_client = client.APIClient(credential)
+
+    def _get_detail(self):
+        if self.dataset_id is None:
+            return
+        try:
+            self.detail = self.api_client.get_dataset(self.dataset_id)['Result']
+        except Exception as e:
+            logging.error('get datasets detail failed, error: %s', e)
+            raise Exception('invalid datasets') from e
+
+    def _get_storage_path(self) -> str:
+        if self.detail is None:
+            return ""
+        return self.detail['StoragePath']
+
+    def _manifest_path(self):
+        return os.path.join(self.local_path,
+                            constants.DATASET_LOCAL_METADATA_FILENAME)
+
+    def _download_file(self, url, target_dir, chunk_size=8192):
+        parse_result = urlparse(url)
+        file_path = os.path.join(target_dir, parse_result.path[1:])
+        dir_path, _ = os.path.split(file_path)
+
+        # create file directory
+        try:
+            os.makedirs(dir_path, exist_ok=True)
+        except OSError:
+            logging.warning('Cannot create download directory: %s', dir_path)
+
+        # download file base on url schemes
+        if parse_result.scheme == 'https' or parse_result.scheme == 'http':
+            # write response chunks in file
+            with requests.get(url, stream=True) as r:
+                r.raise_for_status()
+                with open(file_path, 'wb+') as f:
+                    for chunk in r.iter_content(chunk_size=chunk_size):
+                        f.write(chunk)
+        elif parse_result.scheme == 'tos':
+            bucket = parse_result.netloc.split('.')[0]
+            key = parse_result.path[1:]
+            self.tos_client.download_file(file_path, bucket, key)
+        else:
+            logging.warning('Cannot handle url scheme: %s', url)
+            raise requests.exceptions.InvalidURL
+
+        return file_path
+
+    def get_paths(self, offset=0, limit=-1) -> Tuple[List, List]:
+        """get filepaths of dataset files
+
+        Args:
+            offset (int, optional): num of images to skip. Defaults to 0.
+            limit (int, optional): num of images to load. Defaults to -1.
+
+        Returns:
+            list of paths. Single tabular_path will be returned if it is a TabularDataset
+            list of annotations. No annotations for TabularDataset
+        """
+        if not self.tabular_path:
+            return [self.tabular_path], None
+        paths = []
+        annotations = []
+
+        with open(self._manifest_path()) as f:
+            for i, line in enumerate(f):
+                manifest_line = json.loads(line)
+                if i < offset:
+                    continue
+                if limit != -1 and i >= offset + limit:
+                    break
+                file_path = manifest_line['data']['FilePath']
+                paths.append(file_path)
+                annotations.append(manifest_line['annotation'])
+
+        return paths, annotations
