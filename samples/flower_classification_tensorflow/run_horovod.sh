8d7db239cec3bdbee432839354ca38c86a6f9619
leijingyan <leijingyan@bytedance.com>
2021-09-24 22:08:05 +0800

    feat: add sample yaml for cli

--- samples/flower_classification_tensorflow/run_horovod.sh
+++ samples/flower_classification_tensorflow/run_horovod.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 FILE_PATH=$(cd "$(dirname "$0")";pwd)
 cd $FILE_PATH
+
 export PYTHONPATH="../../"
-horovodrun -np 2 -H "${ML_PLATFORM_MPI_HOSTS}" python swin_transformer_horovod.py
+
+horovodrun -np 3 -H "${ML_PLATFORM_MPI_HOSTS}" python swin_transformer_horovod.py
