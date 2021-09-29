29469cc7280f5238c6c91e8881e553c3f3278713
leijingyan <leijingyan@bytedance.com>
2021-09-29 23:08:48 +0800

    feat: add byteps examples

--- samples/flower_classification_tensorflow/run_horovod.sh
+++ samples/flower_classification_tensorflow/run_horovod.sh
@@ -5,4 +5,6 @@ cd $FILE_PATH
 
 export PYTHONPATH="../../"
 
-horovodrun -np 3 -H "${ML_PLATFORM_MPI_HOSTS}" python swin_transformer_horovod.py
+horovodrun -np ML_PLATFORM_MPI_NP -H "${ML_PLATFORM_MPI_HOSTS}" python swin_transformer_horovod.py || true
+
+sleep 1d
